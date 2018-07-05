e021064886c11435cc171a95ced609d254c75ef9
Catalina Rojas <contacto@ojocorto.com>
2018-07-05 10:08:31 +0200

    arreglando base de datos

--- db/migrate/20180703163020_add_reference_to_peliculas.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToPeliculas < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :peliculas, :fondo, foreign_key: true
-  end
-end
--- db/migrate/20180704092016_remove_reference_to_pelicula.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferenceToPelicula < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :peliculas, :fondo, index: true
-  end
-end
