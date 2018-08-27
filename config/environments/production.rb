9b42c24caca7c52214505967f4c4bd4c7e710a84
Catalina Rojas <contacto@ojocorto.com>
2018-08-27 16:15:47 +0200

    contacto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
    api_key: 'ENV["mailgun_secret_api_key"]',
-   domain: 'mydomain.com',
+   domain: 'sandboxab5b87413cf541f1b078245502bfc2fd.mailgun.org',
   }
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
