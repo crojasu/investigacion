4be2f2a5df18f8ed129bf1aa323e46d7b78d9662
Catalina Rojas <contacto@ojocorto.com>
2018-08-27 17:03:38 +0200

    Asd

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,5 @@
 class MessageMailer < ApplicationMailer
-require 'mailgun-ruby'
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -7,11 +7,7 @@ require 'mailgun-ruby'
   #
   def contacto(message)
     @body = message.body
-    mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
-    message_params = {:from => message.email,
-                      :to => ENV['email'],
-                      :subject => 'Contact Form',
-                      :text => message.body}
-    mg_client.send_message ENV['mailgun_domain'], message_params
+
+    mail to: "ucronia.investiga.actua@gmail.com", from: message.email
   end
 end
