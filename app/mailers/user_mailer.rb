60d86a2297c55aa3fe5f6d051506a8ca863dd4b1
Catalina Rojas <contacto@ojocorto.com>
2018-08-27 10:59:34 +0200

    contactoas

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @body = message.body
     mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
     message_params = {:from => message.email,
-                      :to => ENV['email'],
+                      :to => ENV['rojas.catalina@gmail.com'],
                       :subject => 'Contact Form',
                       :text => message.body}
     mg_client.send_message ENV['mailgun_domain'], message_params
